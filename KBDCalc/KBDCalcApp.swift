c78eee653b2bc9ba14d72d23537cb8a70bbe586c
Wayne Dahlberg <waynefj40@gmail.com>
2024-03-01 13:24:46 -0700

    Initial Commit

---
+++ KBDCalc/KBDCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  KBDCalcApp.swift
+//  KBDCalc
+//
+//  Created by Wayne Dahlberg on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct KBDCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
