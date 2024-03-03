31397053f66886737f76145749c285151e5aaaa1
Wayne Dahlberg <waynefj40@gmail.com>
2024-03-02 20:18:12 -0700

    All orientations and device sizes accounted for; iPhone and iPad only for now.

--- KBDCalc/Notes.md
+++ KBDCalc/Notes.md
@@ -7,3 +7,8 @@ max size for phone is basically take geometry reader of device width and divide 
 height is basically the width, with exception of non-square keys, grid spaces.
 
 
+Aspect ratio for the display is 2.19-ish. Fills entire area on touch-id phones, leaves room for some branding, skewy for iphone-X+ later phones.
+
+Do not waste time right now on horizontal layout. Do that after shipping v1
+
+
--- KBDCalc/SquareKeysTestView.swift
+++ KBDCalc/SquareKeysTestView.swift
@@ -8,32 +8,52 @@
 import SwiftUI
 
 struct SquareKeysTestView: View {
+  @Environment(\.horizontalSizeClass) var horizontalSizeClass
+  @Environment(\.verticalSizeClass) var verticalSizeClass
+  
   var body: some View {
     ZStack {
       Color(.black).edgesIgnoringSafeArea(.all)
       
       GeometryReader { proxy in // whole screen is here
         VStack(spacing: 0) {
-          Rectangle()
-            .fill(.gray)
-            .overlay {
-              GeometryReader { geo in
-                Text("\(String(format: "%.0f", geo.size.width))" + "  ✕  " + "\(String(format: "%.0f", geo.size.height))")
-                  .position(x: geo.size.width / 2, y: geo.size.height / 2)
+          ZStack {
+            RoundedRectangle(cornerRadius: 13.33, style: .continuous)
+              .fill(.gray.opacity(0.15))
+              .ignoresSafeArea(edges: [.top, .bottom])
+              .overlay {
+                GeometryReader { geo in
+                  Text("\(String(format: "%.0f", geo.size.width))" + "  ✕  " + "\(String(format: "%.0f", geo.size.height))")
+                    .position(x: geo.size.width / 2, y: geo.size.height / 2)
+                }
               }
+            
+            VStack {
+              Spacer()
+              
+              RoundedRectangle(cornerRadius: 13.33, style: .continuous)
+                .fill(.red)
+                .aspectRatio(2.19, contentMode: .fit)
+                .frame(width: proxy.size.width)
             }
-          Spacer()
+          }
+          
+          Spacer() // if needed?
+          
           ForEach(0 ..< 5) { row in
             HStack(spacing: 0) {
               ForEach(0..<4, id:\.self) { rect in
                 RoundedRectangle(cornerRadius: 13.33, style: .continuous)
                   .fill(randomColor())
-                  .frame(width: UIDevice.isTablet
-                         ? 128
-                         : proxy.size.width / 4,
-                         height: UIDevice.isTablet
-                         ? 128
-                         : proxy.size.width / 4, alignment: .center)
+                  .frame(width: horizontalSizeClass == .compact ? proxy.size.width / 4 : 128,
+                         height: horizontalSizeClass == .compact ? proxy.size.width / 4 : 128,
+                         alignment: .center)
+//                  .frame(width: UIDevice.isTablet || horizontalSizeClass != .compact
+//                         ? 128
+//                         : proxy.size.width / 4,
+//                         height: UIDevice.isTablet || horizontalSizeClass != .compact
+//                         ? 128
+//                         : proxy.size.width / 4, alignment: .center)
                   .overlay {
                     Text("\(String(format: "%.0f", proxy.size.width / 4))")
                       .foregroundStyle(.black)
@@ -42,7 +62,7 @@ struct SquareKeysTestView: View {
             }
           }
         }
-//        .border(.yellow)
+        //        .border(.yellow)
         .position(x: proxy.size.width / 2, y: proxy.size.height / 2)
       }
     }
