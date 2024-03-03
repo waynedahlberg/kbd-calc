2ed5a53d5b90c04d98cf6f9994818222e067c46c
Wayne Dahlberg <waynefj40@gmail.com>
2024-03-02 18:20:28 -0700

    overlays reporting sizes on all device sizes

--- KBDCalc/SquareKeysTestView.swift
+++ KBDCalc/SquareKeysTestView.swift
@@ -10,34 +10,40 @@ import SwiftUI
 struct SquareKeysTestView: View {
   var body: some View {
     ZStack {
-      VStack {
-        Spacer()
-        
-        GeometryReader { proxy in
-          VStack(spacing: 0) {
-            Spacer()
-            ForEach(0 ..< 5) { row in
-              HStack(spacing: 0) {
-                ForEach(0..<4, id:\.self) { rect in
-                  RoundedRectangle(cornerRadius: 13.33, style: .continuous)
-                    .fill(randomColor())
-                    .frame(width: UIDevice.isTablet
-                           ? 128
-                           : proxy.size.width / 4,
-                           height: UIDevice.isTablet
-                           ? 128
-                           : proxy.size.width / 4, alignment: .center)
-                    .overlay {
-                      Text("\(String(format: "%.0f", proxy.size.width / 4))")
-                        .foregroundStyle(.black)
-                    }
-                }
+      Color(.black).edgesIgnoringSafeArea(.all)
+      
+      GeometryReader { proxy in // whole screen is here
+        VStack(spacing: 0) {
+          Rectangle()
+            .fill(.gray)
+            .overlay {
+              GeometryReader { geo in
+                Text("\(String(format: "%.0f", geo.size.width))" + "  ✕  " + "\(String(format: "%.0f", geo.size.height))")
+                  .position(x: geo.size.width / 2, y: geo.size.height / 2)
+              }
+            }
+          Spacer()
+          ForEach(0 ..< 5) { row in
+            HStack(spacing: 0) {
+              ForEach(0..<4, id:\.self) { rect in
+                RoundedRectangle(cornerRadius: 13.33, style: .continuous)
+                  .fill(randomColor())
+                  .frame(width: UIDevice.isTablet
+                         ? 128
+                         : proxy.size.width / 4,
+                         height: UIDevice.isTablet
+                         ? 128
+                         : proxy.size.width / 4, alignment: .center)
+                  .overlay {
+                    Text("\(String(format: "%.0f", proxy.size.width / 4))")
+                      .foregroundStyle(.black)
+                  }
               }
             }
           }
-          .border(.yellow)
-          .position(x: proxy.size.width / 2, y: proxy.size.height / 2)
         }
+//        .border(.yellow)
+        .position(x: proxy.size.width / 2, y: proxy.size.height / 2)
       }
     }
   }
